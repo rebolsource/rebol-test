16fef66b1b04c71c3765d4a0ac2764b0c528091c
ladislav <lmecir@volny.cz>
2010-11-01 15:59:38 +0100

    #1549

--- core-tests.r
+++ core-tests.r
@@ -9544,6 +9544,45 @@ Functions/math/sameq.r
 #r3only
 ["foo" = decompress/gzip #{1F8B0800EF46BE4C00034BCBCF07002165738C03000000}]
 functions/context/bind.r
+; BIND works 'as expected' in object spec
+; bug#1549
+[
+	b1: [self]
+	ob: make object! [
+	    b2: [self]
+	    set 'a same? first b2 first bind/copy b1 'b2
+	]
+	a
+]
+; bug#1549
+; BIND works 'as expected' in function body
+[
+	b1: [self]
+	f: func [/local b2] [
+	    b2: [self]
+	    same? first b2 first bind/copy b1 'b2
+	]
+	f
+]
+; bug#1549
+; BIND works 'as expected' in closure body
+[
+	b1: [self]
+	f: closure [/local b2] [
+	    b2: [self]
+	    same? first b2 first bind/copy b1 'b2
+	]
+	f
+]
+; bug#1549
+; BIND works 'as expected' in REPEAT body
+[
+	b1: [self]
+	repeat i 1 [
+	    b2: [self]
+	    same? first b2 first bind/copy b1 'i
+	]
+]
 ; bug#1655
 [not head? bind next [1] 'rebol]
 functions/file/exists.r
