dab52eb415e3de920b1853eb706b180703534d85
ladislav <lmecir@volny.cz>
2012-01-18 14:39:34 +0100

    Flags description added

---
+++ flags.r
@@ -0,0 +1,26 @@
+; the flag influences only the test immediately following it,
+; if not explicitly stated otherwise
+
+#32bit
+; the test is meant to be used only when integers are 32bit
+
+#64bit
+; the test is meant to be used only when integers are 64bit
+
+#r2only
+; the test is not meant to be used with the R3 interpreter
+
+#r3only
+; the test is not meant to be used with the R2 interpreter
+
+#r2crash
+; the test crashes the R2 interpreter
+
+#r3crash
+; the test crashes the R3 interpreter
+
+#test2crash
+; the test crashes the test environment when run in the R2 interpreter
+
+#test3crash
+; the test crashes the test environment when run in the R3 interpreter
