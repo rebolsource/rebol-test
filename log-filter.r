8eedae8f121ecf014cf78535dcb463278a5a57a9
ladislav <lmecir@volny.cz>
2013-09-12 16:30:51 +0200

    Update boilerplates.

--- log-filter.r
+++ log-filter.r
@@ -1,6 +1,14 @@
 Rebol [
 	Title: "Log filter"
 	File: %log-filter.r
+	Copyright: [2012 "Saphirion AG"]
+	License: {
+		Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+	}
 	Author: "Ladislav Mecir"
 	Purpose: "Test framework"
 ]
