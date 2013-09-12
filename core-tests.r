825fddbe835cc4ee149fabb3617e6f603b5d202c
ladislav <lmecir@volny.cz>
2013-09-12 15:03:57 +0200

    Boilerplate added

--- core-tests.r
+++ core-tests.r
@@ -1,4 +1,24 @@
-; datatypes/action.r
+; *****************************************************************************
+; Title: Rebol core tests
+; Copyright:
+;     2012 REBOL Technologies
+;     2013 Saphirion AG
+; Author:
+;     Carl Sassenrath, Ladislav Mecir, Andreas Bolka, Brian Hawley
+; License:
+;     Licensed under the Apache License, Version 2.0 (the "License");
+;     you may not use this file except in compliance with the License.
+;     You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+;     Unless required by applicable law or agreed to in writing, software
+;     distributed under the License is distributed on an "AS IS" BASIS,
+;     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;     See the License for the specific language governing permissions and
+;     limitations under the License.
+; *****************************************************************************
+datatypes/action.r
 [action? :abs]
 [not action? 1]
 [action! = type? :abs]
