cfe6ba97c06d2059e1c85494b217e80fd12ad05b
ladislav <lmecir@volny.cz>
2014-01-17 18:11:34 +0100

    update to integer! tests

--- core-tests.r
+++ core-tests.r
@@ -1676,9 +1676,9 @@
 ; 64bit maximum
 #64bit
 [integer? 9223372036854775807]
-[0 = make integer! 0]
-[0 = make integer! "0"]
-[0 = to integer! 0]
+[0 == make integer! 0]
+[0 == make integer! "0"]
+[0 == to integer! 0]
 [-2147483648 == to integer! -2147483648.0]
 [-2147483648 == to integer! -2147483648.9]
 [2147483647 == to integer! 2147483647.9]
@@ -1689,13 +1689,21 @@
 ; bug#921
 [error? try [to integer! 9.2233720368547765e18]]
 [error? try [to integer! -9.2233720368547779e18]]
-[0 = to integer! "0"]
-[0 = to integer! false]
-[1 = to integer! true]
-[0 = to integer! #"^@"]
-[1 = to integer! #"^a"]
-[0 = to integer! #0]
-[1 = to integer! #1]
+[0 == to integer! "0"]
+[0 == to integer! false]
+[1 == to integer! true]
+[0 == to integer! #"^@"]
+[1 == to integer! #"^a"]
+[0 == to integer! #0]
+[1 == to integer! #1]
+[0 == to integer! #{00}]
+[1 == to integer! #{01}]
+#32bit
+[-1 == to integer! #{ffffffff}]
+#64bit
+[-1 == to integer! #{ffffffffffffffff}]
+#64bit
+[302961000000 == to integer! "3.02961E+11"]
 [error? try [to integer! "t"]]
 ["0" = mold 0]
 ["1" = mold 1]
