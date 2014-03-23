247152cd9d5a9a1f0416a2d86aaa9e6bd6ed8dbb
Andreas Bolka <a@bolka.at>
2014-03-23 16:08:44 +0100

    Add tests for bug#1516
    SORT/compare ignores the typespec of its function argument.

--- core-tests.r
+++ core-tests.r
@@ -12188,6 +12188,8 @@
 	same? head blk skip blk -2147483648
 ]
 ; functions/series/sort.r
+[[1 2 3] = sort [1 3 2]]
+[[3 2 1] = sort/reverse [1 3 2]]
 ; bug#1152: SORT not stable (order not preserved)
 [strict-equal? ["A" "a"] sort ["A" "a"]]
 ; bug#1152: SORT not stable (order not preserved)
@@ -12210,6 +12212,10 @@
 ]
 ; bug#1152: SORT not stable (order not preserved)
 [equal? [1 9 1 5 1 7] sort/skip/compare [1 9 1 5 1 7] 2 1]
+[[1 2 3] = sort/compare [1 3 2] :<]
+[[3 2 1] = sort/compare [1 3 2] :>]
+; bug#1516: SORT/compare ignores the typespec of its function argument
+[sort/compare [1 2 #[unset!]] :>]
 ; functions/series/split.r
 ; Tests taken from bug#1886.
 #r3
