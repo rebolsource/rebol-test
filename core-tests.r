2cd1f9776f1f77108cecafef156a8d2df4cc4970
Andreas Bolka <a@bolka.at>
2014-05-12 11:25:51 +0200

    Add MD5/SHA1 tests with slightly longer input strings

--- core-tests.r
+++ core-tests.r
@@ -12320,7 +12320,9 @@
 [equal? make typeset! [decimal! integer!] union make typeset! [decimal!] make typeset! [integer!]]
 ; functions/string/checksum.r
 [#{ACBD18DB4CC2F85CEDEF654FCCC4A4D8} = checksum/method to-binary "foo" 'md5]
+[#{FC3FF98E8C6A0D3087D515C0473F8677} = checksum/method to-binary "hello world!" 'md5]
 [#{0BEEC7B5EA3F0FDBC95D0DD47F3C5BC275DA8A33} = checksum/method to-binary "foo" 'sha1]
+[#{430CE34D020724ED75A196DFC2AD67C77772D169} = checksum/method to-binary "hello world!" 'sha1]
 ; bug#1678: "Can we add CRC-32 as a checksum method?"
 #r3only
 [(checksum/method to-binary "foo" 'CRC32) = -1938594527]
