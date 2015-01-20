763dba4a665e7da8980abea35600ff197cc50e11
Andreas Bolka <a@bolka.at>
2015-01-20 01:38:37 +0100

    Add basic vector! tests
    Includes tests for cc#1213 and cc#1538.

--- core-tests.r
+++ core-tests.r
@@ -3136,6 +3136,30 @@
 [equal? mold/all #[url! "a"] {#[url! "a"]}]
 ; bug#2011
 [not equal? load "http://a.b.c/d?e=f%26" load "http://a.b.c/d?e=f&"]
+; datatypes/vector.r
+[vector? make vector! 0]
+[vector? make vector! [integer! 8]]
+[vector? make vector! [integer! 16]]
+[vector? make vector! [integer! 32]]
+[vector? make vector! [integer! 64]]
+[0 = length? make vector! 0]
+[1 = length? make vector! 1]
+[1 = length? make vector! [integer! 32]]
+[2 = length? make vector! 2]
+[2 = length? make vector! [integer! 32 2]]
+; bug#1538
+[10 = length? make vector! 10.5]
+; bug#1213
+[error? try [make vector! -1]]
+[0 = first make vector! [integer! 32]]
+[all map-each x make vector! [integer! 32 16] [zero? x]]
+[
+	v: make vector! [integer! 32 3]
+	v/1: 10
+	v/2: 20
+	v/3: 30
+	v = make vector! [integer! 32 [10 20 30]]
+]
 ; datatypes/word.r
 [word? 'a]
 [not word? 1]
